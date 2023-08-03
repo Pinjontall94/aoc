87f7d53b36f0291b2a993f18ea0c5eda0cf75a47
Sammi Johnson <sambjohnson94@protonmail.com>
2023-08-02 21:34:56 -0700

    trying to get this thing to work ;-;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, Read, self};
+// mod aoc2020;
+
+
+fn main() -> io::Result<(), std::io::Error>{
+    let mut input = String::new();
+    stdin().lock().read_to_string(&mut input)?;
+    // let res = aoc2020::day01::solve(&input);
+    println!("Hey girl! :3");
 }
